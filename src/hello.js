99cb77257105e2fec59906ad787250293857ba21
Your Name <you@example.com>
2019-12-15 19:30:09 +0100

    Wip

---
+++ src/hello.js
@@ -0,0 +1,6 @@
+ 
+
+export const hello = word => console.log(word, variable);
+export const helloToBrows = word => document.body.textContent = word + variable;
+
+const variable = " and variable"
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { hello, helloToBrows } from './hello';
+
+
+hello("hello from webpack");
+helloToBrows("hope that it's working ;)")
